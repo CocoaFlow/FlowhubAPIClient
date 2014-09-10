603fcdeb55243e47204b6707e4dc07c84738ecb7
Paul Young <paulyoungonline@gmail.com>
2014-09-09 23:41:34 -0400

    Initial Commit

---
+++ FlowhubAPIClient/FlowhubAPIClient.h
@@ -0,0 +1,19 @@
+//
+//  FlowhubAPIClient.h
+//  FlowhubAPIClient
+//
+//  Created by Paul Young on 09/09/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FlowhubAPIClient.
+FOUNDATION_EXPORT double FlowhubAPIClientVersionNumber;
+
+//! Project version string for FlowhubAPIClient.
+FOUNDATION_EXPORT const unsigned char FlowhubAPIClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowhubAPIClient/PublicHeader.h>
+
+
