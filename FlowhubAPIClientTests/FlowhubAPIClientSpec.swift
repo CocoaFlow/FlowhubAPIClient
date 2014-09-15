acf298431021f8320563a808e15453683177d2ca
Paul Young <paulyoungonline@gmail.com>
2014-09-15 02:19:02 -0400

    Add spec file to test targets.

---
+++ FlowhubAPIClientTests/FlowhubAPIClientSpec.swift
@@ -0,0 +1,21 @@
+//
+//  FlowhubAPIClientSpec.swift
+//  FlowhubAPIClient
+//
+//  Created by Paul Young on 15/09/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+class FlowhubAPIClientSpec: QuickSpec {
+    
+    override func spec() {
+        
+        describe("Flowhub API client") {
+            
+        }
+    }
+}
